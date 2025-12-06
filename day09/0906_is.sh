1c2d1c735b48f9cce028816538556f098df91823
ConniConni <nmtgktmo@gmail.com>
2025-12-06 22:19:26 +0900

    ifの論理積と論理和

--- day09/0906_is.sh
+++ day09/0906_is.sh
@@ -9,4 +9,35 @@ fi
 num=20
 
 ((num++))
-echo "${num}"
+echo "num=${num}"
+
+# 否定1
+if [[ ! ${num} -gt 23 ]]; then
+  echo "numは23より大きくありません"
+fi
+
+# 否定2
+if !(( ${num} > 23 )); then
+  echo "numは23より大きくありません"
+fi
+
+# OR(論理和)1
+if [[ ${num} > 0 || ${num} < 0 ]]; then
+  echo "${num}は0ではありません"
+fi
+
+# OR(論理和)2
+if ((${num} > 0 || ${num} < 0)); then
+  echo "${num}は0ではありません"
+fi
+
+# AND(論理積)1
+if [[ ${num}>0 && ${num}%2=1 ]]; then
+  echo "${num}は正の奇数です"
+fi
+
+((num++))
+# AND(論理積)2
+if (( ${num}>0 && ${num}%2==0 )); then
+  echo "${num}は正の偶数です"
+fi
