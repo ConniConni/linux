6ca34d229c3cfb8f9c340676acf59aac5bf12b3d
ConniConni <nmtgktmo@gmail.com>
2025-12-12 20:31:23 +0900

    if内の正規表現

--- day09/0908_if_special.sh
+++ day09/0908_if_special.sh
@@ -2,6 +2,7 @@
 
 export locky_color="red"
 export locky_color2="blue"
+export var=12345
 
 # 変数が設定されていない場合
 
@@ -25,6 +26,15 @@ echo "ラッキーカラーは${locky_color2}です"
 if [[ -n "${locky_color}" ]]; then
 	echo "ラッキーからは${locky_color}です"
 fi
+
+# ${var}が1から始まる場合の処理を記述
+if [[ "${var}" == 1* ]]; then
+	echo "${var}は1から始まります"
+else
+	echo "${var}は1から始まりません"
+fi
+
+
 
 
 # 環境変数を削除
