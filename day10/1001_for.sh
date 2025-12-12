df9594a417ed929a15c8e347fe7f370ac4f713c6
ConniConni <nmtgktmo@gmail.com>
2025-12-12 22:06:59 +0900

    数字を使ったforループ

--- day10/1001_for.sh
+++ day10/1001_for.sh
@@ -9,3 +9,33 @@ do
 	echo "${name}"
         echo $(( count++ ))
 done
+
+# for文で5回繰り返す
+echo "for文で5回繰り返す"
+for i in {1..5}
+do
+	echo "${i}"
+done
+
+# for文で偶数を0から10まで表示
+echo "for文で偶数を0から10まで表示"
+for even_num in {0..10..2}
+do
+	echo "${even_num}"
+done
+
+# for文で正の奇数を11から表示
+echo "for文で正の奇数を11から表示"
+
+for odd_num in {11..0..2}
+do
+	echo "${odd_num}"
+done
+
+# for文で正の奇数を11から表示
+echo "for文で正の奇数を11から表示"
+
+for odd_num in  $( seq 11 -2 0)
+do
+        echo "${odd_num}"
+done
